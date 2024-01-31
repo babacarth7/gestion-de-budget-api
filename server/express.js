94f8749b91ddb1dfedf1cc1aba034e5aaf9ba093
Babacar Thiam <todos-mop-0q@icloud.com>
2024-01-31 16:48:45 +0000

    impress express.js file

--- server/express.js
+++ server/express.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
@@ -9,13 +8,15 @@ import BudgetRoutes from '../routes/budget.routes.js';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: true }))
-app.use(cookieParser())
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(compress());
 app.use(cors());
 app.use(helmet());
 
+// Routes
 app.use('/', BudgetRoutes);
 
 export default app;
